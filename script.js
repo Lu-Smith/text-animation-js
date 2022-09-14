c4a3ba5be81f8d947e48658cbdbdb2cceff6e55c
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-09-14 14:23:13 +0100

    Updated script.js

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ ctx1.fillStyle = 'white';
 ctx1.font = '90px Verdana';
 ctx1.fillText('Hello', 140, 220);
 
-const data = ctx1.getImageData(0, 0, canvas1.width, canvas1.height);
+const data1 = ctx1.getImageData(0, 0, canvas1.width, canvas1.height);
 
 class Particle1 {
     constructor(x, y){
@@ -113,13 +113,88 @@ function init1() {
 
 init1();
 
-function animate() {
+function animate1() {
     ctx1.clearRect(0, 0, canvas1.width, canvas1.height);
     for (let i = 0; i < particlesArray1.length; i++) {
         particlesArray1[i].draw();
         particlesArray1[i].update();
     }
-    requestAnimationFrame(animate);
+    requestAnimationFrame(animate1);
 }
 
-animate();
+animate1();
+
+//camvas2
+const canvas2 = document.getElementById('canvas2');
+const container2 = document.getElementById('container2');
+const ctx2 = canvas2.getContext('2d');
+const particlesArray2 = [];
+
+canvas2.width = container2.offsetWidth;
+canvas2.height = container2.offsetHeight;
+
+const mouse2 = {
+    x: null,
+    y: null,
+    radius: 150
+}
+
+canvas2.addEventListener('mousemove', function(event) {
+    mouse2.x = event.x - canvas2.getBoundingClientRect().left;
+    mouse2.y = event.y - canvas2.getBoundingClientRect().top;
+})
+
+ctx2.fillStyle = 'white';
+ctx2.font = '90px Verdana';
+ctx2.fillText('Hello', 140, 220);
+
+const data2 = ctx2.getImageData(0, 0, canvas2.width, canvas2.height);
+
+class Particle2 {
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+        this.size = 3;
+        this.baseX = this.x;
+        this.baseY = this.y;
+        this.density = (Math.random() * 30) + 1;
+    }
+    draw(){
+        ctx2.fillStyle = 'red';
+        ctx2.beginPath();
+        ctx2.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx2.closePath();
+        ctx2.fill();
+    }
+    update(){
+        let dx = mouse2.x - this.x;
+        let dy = mouse2.y - this.y;
+        let distance = Math.sqrt(dx * dx + dy * dy);
+        if (distance < 100) {
+            this.size = 10;
+        } else {
+            this.size = 3;
+        }
+    }
+}
+
+function init2() {
+    for (let i = 0; i < 500; i++) {
+        let x = Math.random() * canvas2.width;
+        let y = Math.random() * canvas2.height;
+        particlesArray2.push(new Particle2(x, y));
+    }
+}
+
+init2();
+
+function animate2() {
+    ctx2.clearRect(0, 0, canvas2.width, canvas2.height);
+    for (let i = 0; i < particlesArray2.length; i++) {
+        particlesArray2[i].draw();
+        particlesArray2[i].update();
+    }
+    requestAnimationFrame(animate2);
+}
+
+animate2();
