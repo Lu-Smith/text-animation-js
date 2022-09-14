96408ae6bde3f03baaf696dcc2befe67eaefd99f
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-09-14 13:20:18 +0100

    Added animation to subtitle

--- script.js
+++ script.js
@@ -19,3 +19,32 @@ function checkBoxes() {
         }
     })
 }
+
+//subtitle
+const text = document.getElementById('subtitle');
+const strText = text.textContent;
+const splitText = strText.split("");
+text.textContent = "";
+
+for (let i = 0; i < splitText.length; i++) {
+    text.innerHTML += "<span>" + splitText[i] + "</span>";
+}
+
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick(){
+    const spans = text.querySelectorAll('span')[char];
+    spans.classList.add('fade');
+    char++
+    if(char === splitText.length) {
+        complete();
+        return;
+    }
+}
+
+function complete() {
+    clearInterval(timer);
+    timer = null;
+
+}
